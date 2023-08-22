3e9b03f28997100bb633a37c36c4dada87b37083
Amish <hajiaamish533@gmail.com>
2023-08-22 09:39:26 -0700

    my first drum kit

---
+++ index.js
@@ -0,0 +1,64 @@
+var NumberOfDrum = document.querySelectorAll(".drum").length
+for (var i = 0; i < NumberOfDrum; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click",HandleClick)
+    
+}
+
+function HandleClick() {
+    var buttonInnerHTML = this.innerHTML
+    MakeSound(buttonInnerHTML)
+    buttonAnimation(buttonInnerHTML)
+
+ 
+}
+document.addEventListener("keypress", function(event) {
+    MakeSound(event.key)
+    buttonAnimation(event.key)
+    
+})
+
+function MakeSound(key) {
+    switch (key) {
+        case "w":
+                var tom1 = new Audio('sounds/tom-1.mp3');
+             tom1.play();
+        break;
+        case "a":
+                var tom2 = new Audio('sounds/tom-2.mp3');
+                tom2.play();
+        break;
+        case "s":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+         break;
+         case "d":
+             var tom4 = new Audio('sounds/tom-4.mp3');
+             tom4.play();
+        break;
+        case "j":
+            var snare = new Audio('sounds/snare.mp3');
+         snare.play();
+        break;
+        case "k":
+            var crash = new Audio('sounds/crash.mp3');
+         crash.play();
+        break;
+        case "l":
+            var kick = new Audio('sounds/kick-bass.mp3');
+         kick.play();
+        break;
+        
+    
+        defaul:console.log(buttonInnerHTML);
+            
+    }
+    
+}
+function buttonAnimation(CurrentKey) {
+   var activebutton = document.querySelector("." + CurrentKey).classList.add("pressed")
+   setTimeout(function() {
+    document.querySelector("." + CurrentKey).classList.remove("pressed")
+   },100);
+}
+
+    
